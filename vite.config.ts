c42714a6f6948cfd760784b56ffb785e0b37df71
Sara Mahmodi <saramah63@gmail.com>
2026-03-06 09:16:51 +0200

    fix vercel middleware

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __dirname: JSON.stringify('/'),
+    __filename: JSON.stringify('/index.html'),
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
